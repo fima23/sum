3b91069bd162ccf8b3c21d2f5768f9c35d330b5c
yankouskia <aleksandr.yankovskiy@gmail.com>
2017-02-06 19:37:35 +0300

    initial tests

---
+++ test.js
@@ -0,0 +1,26 @@
+const assert = require('assert');
+const sum = require('./src/index.js');
+
+it('easy test', () => {
+  assert.equal((sum(1 ,2)), 3);
+});
+
+it('medium test', () => {
+  assert.equal((sum(123456789, 987654321)), 1111111110);
+});
+
+it('hard test', () => {
+  assert.equal(
+    (
+      sum(
+        111111111111111111111111111111111111111111111111111111111111,
+        222222222222222222222222222222222222222222222222222222222222
+      )
+    ),
+    333333333333333333333333333333333333333333333333333333333333);
+});
+
+it('bad test', () => {
+  assert.equal(1, 1);
+  assert.notEqual((sum(null, null)), (sum(null, null)));
+});
